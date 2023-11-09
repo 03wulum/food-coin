3f528305295f532fd92ed11c81c1f276b0ea9dc2
Obinna Ewulum <obinnaewulum@gmail.com>
2023-11-09 01:20:20 -0500

    Create entry point for new coin

---
+++ README.md
@@ -0,0 +1,5 @@
+# Welcome to the Food Coin Project
+
+## The aim of this cryptocurrency is to end world hunger.
+
+### To do this, we will use a percentage of each transaction to donate to the global organizations whose main purpose is to feed the less fortunate.
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "block.h"
+#include "BlockChain.h"
+#include "Transaction.h"
+#include "Wallet.h"
+
+int main() {
+    //create a new blockchain
+    Blockchain blockchain;
+
+    //create a new wallet;
+
+    //create a new transaction
+}
