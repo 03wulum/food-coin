fc0f3de5d587341f98f07e2e1c2e4d2d84337255
Obinna Ewulum <obinnaewulum@gmail.com>
2023-11-10 22:23:50 -0500

    use evp, clean up code, remove deprecate openssl older hash sha functions.

--- src/block.cpp
+++ src/block.cpp
@@ -1,13 +1,11 @@
 #include "block.h"
-#include <openssl/sha.h>
+#include <openssl/evp.h>
 #include <sstream>
 #include <iomanip>
 
-// extern "C" {
-// #include <openssl/sha.h>
-// }
 
-Block::Block(int index, std::string previousHash, std::vector<std::string> transactions, long timestamp, int nonce) {
+Block::Block(int index, std::string previousHash, std::vector<std::string> transactions, long timestamp, int nonce)
+{
     this->index = index;
     this->previousHash = previousHash;
     this->transactions = transactions;
@@ -15,68 +13,49 @@ Block::Block(int index, std::string previousHash, std::vector<std::string> trans
     this->nonce = nonce;
 
     this->hash = calculateHash();
-
-}
-
-// std::string Block::calculateHash() {
-//     std::string source = this->previousHash;
-//     for(const auto& transaction : this->transactions) {
-//         source += transaction;
-//     }
-//     source += std::to_string(this->nonce) + std::to_string(this->timestamp);
-
-//     std::string digest;
-//     CryptoPP::SHA256 hash;
-
-//     CryptoPP::StringSource s(source, true, 
-//         new CryptoPP::HashFilter(hash, 
-//             new CryptoPP::HexEncoder(
-//                 new CryptoPP::StringSink(digest)
-//             ) // HexEncoder
-//         ) // HashFilter
-//     ); // StringSource
-
-//     return digest;
-// }
-
-std::string Block::calculateHash() {
-    //SHA context to hold state of the computation
-    SHA256_CTX sha256;
-    //initialize it sha256 structure
-    SHA256_Init(&sha256);
-    /*do an update using the initialized sha256 and the previous hash
-    ** but you need to turn the previousHash of the block to a c string and include the length in the update.
-    */
-    SHA256_Update(&sha256, this->previousHash.c_str(), this->previousHash.size()); //this is importand for incremental hashing of chunks of data in a stream
-    /**
-     * update sha structure with each transaction in the block
-    */
-    for(std::string transaction : this->transactions) {
-        //size to get length of string
-        SHA256_Update(&sha256, transaction.c_str(), transaction.size());
-    }
-    //& operators is used to get the address of the variables.
-    // cast int variables, nonce and timestamp, to characters before passing to sha update
-    SHA256_Update(&sha256, (char*)&this->nonce, sizeof(this->nonce));
-    SHA256_Update(&sha256, (char*)&this->timestamp, sizeof(this->timestamp));
-
-    //store the digest, or resulting hash in an array; #32 bytes for SHA-256
-    unsigned char hash[SHA256_DIGEST_LENGTH];
-
-    //finalize hash computation, culminating in the update of the hash array
-    SHA256_Final(hash, &sha256);
-
-    //creates string stream to convert hash array from binary to hexadecimal string
-    std::stringstream ss;
-
-    //convert binary sha256 hash to its hexadecimal representaion, a string representing the hash
-    for(int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
-        ss << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
-    }
-
-    return ss.str();
 }
 
 std::string Block:: getHash() {
     return this->hash;
+}
+
+std::string Block::calculateHash()
+{
+    // Create a buffer to store the hash
+    unsigned char hash[EVP_MAX_MD_SIZE];
+    unsigned int hash_len;
+
+    // Create a new EVP_MD_CTX
+    EVP_MD_CTX *mdctx = EVP_MD_CTX_new();
+
+    // Initialize the EVP_MD_CTX with SHA256
+    EVP_DigestInit_ex(mdctx, EVP_sha256(), NULL);
+
+    // Update the EVP_MD_CTX with the previousHash
+    EVP_DigestUpdate(mdctx, this->previousHash.c_str(), this->previousHash.size());
+
+    // Update the EVP_MD_CTX with each transaction
+    for (std::string transaction : this->transactions)
+    {
+        EVP_DigestUpdate(mdctx, transaction.c_str(), transaction.size());
+    }
+
+    // Update the EVP_MD_CTX with the nonce and timestamp
+    EVP_DigestUpdate(mdctx, (char *)&this->nonce, sizeof(this->nonce));
+    EVP_DigestUpdate(mdctx, (char *)&this->timestamp, sizeof(this->timestamp));
+
+    // Finalize the hash computation
+    EVP_DigestFinal_ex(mdctx, hash, &hash_len);
+
+    // Clean up
+    EVP_MD_CTX_free(mdctx);
+
+    // Convert the hash to a hexadecimal string
+    std::stringstream ss;
+    for (int i = 0; i < hash_len; i++)
+    {
+        ss << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
+    }
+
+    return ss.str();
 }
