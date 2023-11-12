8a8bee70857df171159c05a6b44c9b3899fadae6
Obinna Ewulum <obinnaewulum@gmail.com>
2023-11-11 21:58:34 -0500

    begin work on transaction signing implementationusing evp

--- src/transaction.cpp
+++ src/transaction.cpp
@@ -19,6 +19,30 @@ std::string Transaction::toString() {
 }
 
 void Transaction::sign(std::string privateKey) {
+    //use EVP library from open ssl in declaring a pointer to EVO structure
+    // hold state of message digest
+    EVP_MD_CTX *mdctx = NULL;
+    // *sigholds digital signture array
+    unsigned char *sig = Null;
+    //hold the length of the signature
+    size_t slen;
+
+    // create digest context
+    if(!(mdctx = EVP_MD_CTX_new())) handleErrors() //implement handle errors
+
+    //Load private key
+    BIO *bio = BIO_new_mem_buf(privateKey.data(), privateKey.size());
+    //check if bio object was created
+    if(!bio) handleErrors();
+    //read privatekey from BIO in evep structure
+    EVP_PKEY *pkey = PEM_read_bio_PrivateKey(bio, nullptr, nullptr, nullptr);
+    //check if pkey exist: if it private key was read succesfully
+    if(!pkey) handleErrors();
+
+    //initial signing of digest
+    //we set the mdctx (our hash?) to the EVPSHA-256 hash function;
+    // and the private key to the pkey
+    if(1 != EVP_DigestSignInit(mdctx, NULL, EVP_sha256(), NULL, pkey)) handleErrors();
     
 }
 
